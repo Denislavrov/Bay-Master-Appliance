0a230ce99b28f55fbbc3a7a06752be3dd11dbbaf
Denis Lavrov <d.lavrov@corp.mail.ru>
2022-01-19 00:08:25 +0300

    fixed init

---
+++ html/templates/index.php
@@ -0,0 +1,11 @@
+<?php
+$PAGE['meta_title'] = "Home";
+$page = 'index';
+include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/header.tpl.php");
+?>
+
+<div></div>
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/footer.tpl.php");
+?>
