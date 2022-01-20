6ea3424b6fcafab0051d27a7b610b1a388066191
Denis Lavrov <d.lavrov@corp.mail.ru>
2022-01-20 23:23:28 +0300

    add new page

--- html/templates/index.php
+++ html/templates/index.php
@@ -4,7 +4,21 @@ $page = 'index';
 include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/header.tpl.php");
 ?>
 
-<div></div>
+<div class="service-main">
+    <div class="layout-main">
+        <div class="service-block">
+            <div class="service-title">Lorem ipsum dolor sit amet, consectetur adipiscing elit</div>
+            <div class="service-text">
+                We have experience in the repair of all types and brands of appliances and ready
+                to share it with you. We have experience in the repair.
+            </div>
+            <div class="service-button-block">
+                <button class="service-button">Request service</button>
+                <a href="#" class="service-link">Read more</a>
+            </div>
+        </div>
+    </div>
+</div>
 
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/footer.tpl.php");
--- html/templates/main/header.tpl.php
+++ html/templates/main/header.tpl.php
@@ -1,93 +1,99 @@
 <?php include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/header.tpl.php") ?>
 <div class="main-container">
-<header class="main-header">
-    <div class="js-menu header-mobile-menu__wrap">
-        <ul class="header-mobile-menu__list">
-            <li class="header-mobile-menu__el">
-                <a href="/" class="header-mobile-menu__link">Home</a>
-            </li>
-            <li class="header-mobile-menu__el">
-                <a href="/" class="header-mobile-menu__link">Premium</a>
-            </li>
-            <li class="header-mobile-menu__el">
-                <a href="/" class="header-mobile-menu__link">Blog</a>
-            </li>
-            <li class="header-mobile-menu__el">
-                <a href="/" class="header-mobile-menu__link">About us</a>
-            </li>
-        </ul>
-		    <a
-				    href="#request-popup"
-				    class="js-openDialogAjax btn btn_attention mobile"
-				    data-title="Have you recognize your problem?"
-		    >
-			    Request service
-		    </a>
-        <div class="header-mobile-menu__block">
-            <div class="header-mobile-menu__working">
-                <div>Mon - Fri</div>
-                <div>8am - 6pm</div>
-            </div>
-            <div class="header-mobile-menu__weekend">
-                <div>Saturday - Sunday</div>
-                <div>Closed</div>
-            </div>
-        </div>
-    </div>
-    <div class="js-bg header-mobile-menu__bg" onclick="toggleMenu()"></div>
-    <div class="header-main-wrap">
+    <header class="main-header">
         <div class="layout-main">
-        <div class="header-common">
-            <div class="header-left">
-                <a href="/" class="header-icon">
-                    <img src="/skin/images/svg-sprite/newlogo.svg" alt="icon">
-                </a>
-                <ul class="header-menu">
-                    <li class="header-menu-el">
-                        <a href="/" class="header-menu-link">Home</a>
-                    </li>
-                    <li class="header-menu-el">
-                        <a href="/" class="header-menu-link">Premium</a>
-                    </li>
-                    <li class="header-menu-el">
-                        <a href="/" class="header-menu-link">Blog</a>
-                    </li>
-                    <li class="header-menu-el">
-                        <a href="/" class="header-menu-link">About us</a>
-                    </li>
-                </ul>
-            </div>
-            <div class="header-right">
-                <a href="#" class="header-contact">(408) 484-4353</a>
-                <a class="header-mobile-menu" onclick="toggleMenu()">
-                    <span>Menu</span>
-                    <div class="header-burger">
-                        <span class="header-burger-line"></span>
-                        <span class="header-burger-line"></span>
-                        <span class="header-burger-line"></span>
-                    </div>
-                </a>
+            <div class="header-block">
+                <div class="header-content">
+                    <a href="#" class="header-link">
+                        <img src="/skin/images/Group%20579.svg" alt="logo"/>
+                    </a>
+                    <a href="tel:4084844353" class="header-phone">
+                        <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <circle cx="9.5" cy="9.5" r="9.5" fill="#333333"/>
+                            <path d="M14.3788 12.4611L12.1721 10.4547C12.0678 10.3599 11.9307 10.3094 11.7898 10.3137C11.649 10.3181 11.5153 10.377 11.417 10.4781L10.118 11.8141C9.80527 11.7543 9.17664 11.5584 8.52955 10.9129C7.88247 10.2653 7.6865 9.63503 7.62841 9.32452L8.96329 8.02492C9.06449 7.92673 9.12351 7.79302 9.12787 7.65209C9.13223 7.51115 9.08158 7.37405 8.98664 7.2698L6.98078 5.06363C6.8858 4.95905 6.7538 4.89562 6.61281 4.8868C6.47181 4.87798 6.33293 4.92448 6.22567 5.0164L5.04767 6.02666C4.95381 6.12085 4.89779 6.24622 4.89024 6.37898C4.8821 6.51469 4.72684 9.72949 7.21964 12.2234C9.39432 14.3975 12.1184 14.5566 12.8686 14.5566C12.9783 14.5566 13.0456 14.5533 13.0635 14.5522C13.1962 14.5448 13.3215 14.4885 13.4153 14.3943L14.425 13.2157C14.5173 13.1088 14.5641 12.97 14.5555 12.829C14.5469 12.6881 14.4835 12.556 14.3788 12.4611Z" fill="white"/>
+                        </svg>
+                        (408) 484-4353
+                    </a>
+                    <button class="header-button">Book online</button>
+                </div>
 
-
-		            <a
-				            href="#request-popup"
-				            class="js-openDialogAjax btn btn_attention header"
-				            data-title="Have you recognize your problem?"
-		            >
-			            Request service
-		            </a>
+                <div class="header-menu__wrap">
+                    <nav class="header-menu__list">
+                        <li class="header-menu__el _hasSubmenu">
+                            <a href="/" class="header-menu__link _action">
+                                Services
+                                <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5 8L0.669875 0.5L9.33013 0.500001L5 8Z" fill="#333333"/>
+                                </svg>
+                            </a>
+                            <ul class="header-submenu">
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Refrigerators</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Ovens | Microwave</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Ranges | Cooktops</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Dishwashers</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Washers</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Dryers</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Stackable</a>
+                                </li>
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Garbage disposals</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header-menu__el _hasSubmenu">
+                            <a href="/" class="header-menu__link">
+                                About us
+                                <svg width="10" height="8" viewBox="0 0 10 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M5 8L0.669875 0.5L9.33013 0.500001L5 8Z" fill="#333333"/>
+                                </svg>
+                            </a>
+                            <ul class="header-submenu _one_el">
+                                <li class="header-submenu__el">
+                                    <a href="/" class="header-submenu__link">Reviews</a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="header-menu__el">
+                            <a href="/" class="header-menu__link">Contacts</a>
+                        </li>
+                        <li class="header-menu__el">
+                            <a href="/" class="header-menu__link">Areas</a>
+                        </li>
+                    </nav>
+                    <!--<div class="js-menu header-mobile-menu__wrap">
+                    <nav class="header-mobile-menu__list">
+                        <li class="header-mobile-menu__el">
+                            <a href="/" class="header-mobile-menu__link">Services</a>
+                        </li>
+                        <li class="header-mobile-menu__el">
+                            <a href="/" class="header-mobile-menu__link">About us</a>
+                        </li>
+                        <li class="header-mobile-menu__el">
+                            <a href="/" class="header-mobile-menu__link">Contacts</a>
+                        </li>
+                        <li class="header-mobile-menu__el">
+                            <a href="/" class="header-mobile-menu__link">Areas</a>
+                        </li>
+                    </nav>
+                </div>-->
+                </div>
             </div>
         </div>
-    </div>
-    </div>
-</header>
+    </header>
 
-<?php if ($page!='index') {?>
-    <main
-			class="main-center mt-80<?=($page=='location') ? ' page-bckg page-bckg_location' : ''?><?=($page=='repair' || $page=='blog_article') ? ' full-height' : ''?>"
-			<?=($page=='location') ? 'style="background-image: url(/skin/images/location-bckg.png);"' : '' ?>
-		>
-<?php }?>
-        <?php if ($page!='index' && $page!='repair' && $page!='about_us' && $page!='blog_article') {?>
-	        <div class="layout-main">
-        <?php }?>
+
+<main>
+
