7f7e499869800663d9e367c1bffae8717110d072
Denis Lavrov <d.lavrov@corp.mail.ru>
2022-01-24 16:44:35 +0300

    area added

---
+++ html/templates/area.php
@@ -0,0 +1,80 @@
+<?php
+$PAGE['meta_title'] = "Area";
+$page = 'area';
+include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/header.tpl.php");
+?>
+<section class="area">
+    <div class="layout-main">
+        <div class="area-text-block">
+            <div class="area-title">Our Service Areas</div>
+            <div class="area-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</div>
+        </div>
+        <div class="area-wrap">
+            <div class="area-item _big-item">
+                <img src="/skin/images/Rectangle%201081.png" alt="city"/>
+                <div class="area-list">
+                    <div class="area-list-title">San Jose</div>
+                    <ul class="area-list-items">
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="area-item">
+                <img src="/skin/images/Rectangle%201081.png" alt="city"/>
+                <div class="area-list">
+                    <div class="area-list-title">San Jose</div>
+                    <ul class="area-list-items">
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="area-item">
+                <img src="/skin/images/Rectangle%201081.png" alt="city"/>
+                <div class="area-list">
+                    <div class="area-list-title">San Jose</div>
+                    <ul class="area-list-items">
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="area-item">
+                <img src="/skin/images/Rectangle%201081.png" alt="city"/>
+                <div class="area-list">
+                    <div class="area-list-title">San Jose</div>
+                    <ul class="area-list-items">
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="area-item">
+                <img src="/skin/images/Rectangle%201081.png" alt="city"/>
+                <div class="area-list">
+                    <div class="area-list-title">San Jose</div>
+                    <ul class="area-list-items">
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                        <li class="area-list-item">Refrigerator Repair</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . "/html/templates/main/footer.tpl.php");
+?>
